df6478629755203225484ef8abcb4d2281d5e7bc
antonvancev <95409882+antonvancev@users.noreply.github.com>
2023-02-07 22:20:43 +0500

    add 3

--- Basket.java
+++ Basket.java
@@ -4,7 +4,7 @@ public class Basket {
     private String items;
     private int totalPrice = 0;
     private int limit;
-    private double totalWeight = 2;
+    private double totalWeight = 3;
 
     public Basket() {
         increaseCount(1);
